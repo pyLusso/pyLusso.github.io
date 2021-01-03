151e5cb7759cff8e2256e5611b78c13c75e5ac18
pyLusso <55640983+pyLusso@users.noreply.github.com>
2021-01-03 13:47:48 +1100

    Create sidenav.js
    Sidebar functions for navigating webpage

---
+++ js/sidenav.js
@@ -0,0 +1,17 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("menuB").onclick = function () { closeNav(); threeBar(this) };
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("menuB").onclick = function () { openNav(); threeBar(this) };
+}
+
+function threeBar(x) {
+  x.classList.toggle("change");
+}
